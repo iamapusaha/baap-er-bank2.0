4440ff4e5215866c31fbdc58f35c381f5824b28c
iamapusaha <apusaha392@gmail.com>
2023-08-16 14:20:49 +0600

    added Deposit,Withdraw and Balance section

--- login.js
+++ login.js
@@ -3,10 +3,9 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     const emailValue = emailField.value;
     const passwordField = document.getElementById('user-password')
     const passwordValue = passwordField.value;
-    console.log(emailValue, passwordValue);
     if (emailValue === "apusaha392@gmail.com" && passwordValue === "password123") {
         location.href = 'bank.html'
     } else {
-        console.log('invalid user');
+        alert('toke ami teijjo sontan gosona korlam. tui password vula gacos');
     }
 })
